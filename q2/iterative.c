d2e679149d13c02333745a72a6ecce26f15d0e4f
PoHao0615 <paperhao@hotmail.com>
2015-10-17 23:32:52 +0800

    Implement the iterative method for question2
    1. This iterative method is implemented by binary search.
    2. The example for testing is in the main function.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,29 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
-int main()
-{
-    return 0;
+char smallest_character(char *str, char c) {
+	int l = 0, r = strlen(str) - 1;
+	char ret = str[0];
+	while(l <= r) {
+	        int m = l + (r-l) / 2;
+	        if(str[m] > c) {
+	        	ret = str[m];
+                	r = m - 1;
+        	} else 
+            		l = m + 1;
+        	
+    	}
+    	return ret;
 }
+
+int main(){
+	char str[]={'a','f','g','n','w'},c='p',output;
+    
+    	assert((smallest_character(str,c)) && "function error!\n");
+    	output = smallest_character(str, c);
+    	printf("result : %c\n",output);
+    	return 0;
+}	
