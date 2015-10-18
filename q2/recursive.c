3877fce5f045865d766f464a05b982ca50193ec3
PoHao0615 <paperhao@hotmail.com>
2015-10-18 23:42:59 +0800

    Implement the recursive method for question2
    1. Use the code "if(str[i]!=127)" to determine that the str[] is in the end or not.
    2. The example for testing is in the main function.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,23 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
-int main()
-{
-    return 0;
+char smallest_character(char str[], char c, int i){
+	if(str[i]!=127){
+		if(str[i] > c)
+			return str[i];
+		else
+			return smallest_character(str, c, (i+1));
+	}
+	return str[0];
 }
+
+int main(){
+	char str[]={'a','f','g','n','o'},c='p',output;
+        assert((smallest_character(str, c, 0)) && "function error!\n");
+    	output = smallest_character(str, c, 0);
+    	printf("result : %c\n",output);
+    	return 0;
+}	
